a241eb9a5c91b1055a085a88fe266cdefb08afd3
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-26 09:42:14 -0400

    feat: expose low-level NDS public API

--- src/nds_disassembly_toolkit/nds/__init__.py
+++ src/nds_disassembly_toolkit/nds/__init__.py
@@ -1,3 +1,33 @@
+from nds_disassembly_toolkit.nds.fat import FatEntry, parse_fat
+from nds_disassembly_toolkit.nds.fnt import (
+    ROOT_DIRECTORY_ID,
+    FntDirectory,
+    FntFile,
+    FntTree,
+    parse_fnt,
+)
 from nds_disassembly_toolkit.nds.header import NdsHeader, SectionRange
+from nds_disassembly_toolkit.nds.overlays import (
+    OVERLAY_ENTRY_SIZE,
+    OverlayEntry,
+    parse_arm7_overlays,
+    parse_arm9_overlays,
+    parse_overlay_table,
+)
 
-__all__ = ["NdsHeader", "SectionRange"]
+__all__ = [
+    "FatEntry",
+    "FntDirectory",
+    "FntFile",
+    "FntTree",
+    "NdsHeader",
+    "OVERLAY_ENTRY_SIZE",
+    "OverlayEntry",
+    "ROOT_DIRECTORY_ID",
+    "SectionRange",
+    "parse_arm7_overlays",
+    "parse_arm9_overlays",
+    "parse_fat",
+    "parse_fnt",
+    "parse_overlay_table",
+]
