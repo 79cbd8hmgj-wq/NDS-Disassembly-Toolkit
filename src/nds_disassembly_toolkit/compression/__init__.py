83c44ca156be2a81f216166f96c8e223e0f59934
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-26 09:38:17 -0400

    feat: expose LZ10 compression API

---
+++ src/nds_disassembly_toolkit/compression/__init__.py
@@ -0,0 +1,8 @@
+from nds_disassembly_toolkit.compression.lz10 import (
+    compress_lz10,
+    decompress_lz10,
+    is_lz10,
+    lz10_declared_size,
+)
+
+__all__ = ["compress_lz10", "decompress_lz10", "is_lz10", "lz10_declared_size"]
