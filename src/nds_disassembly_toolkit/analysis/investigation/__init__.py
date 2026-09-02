3ab0fb0d82292a3a315717a760aa1830f0fa68cf
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-09-02 12:41:11 -0400

    feat: export investigation models

---
+++ src/nds_disassembly_toolkit/analysis/investigation/__init__.py
@@ -0,0 +1,17 @@
+"""Deterministic reverse-engineering investigation prioritization."""
+
+from nds_disassembly_toolkit.analysis.investigation.model import (
+    InvestigationCandidate,
+    InvestigationEvidence,
+    InvestigationEvidenceKind,
+    InvestigationReport,
+    InvestigationRequest,
+)
+
+__all__ = [
+    "InvestigationCandidate",
+    "InvestigationEvidence",
+    "InvestigationEvidenceKind",
+    "InvestigationReport",
+    "InvestigationRequest",
+]
